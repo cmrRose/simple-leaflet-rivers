f310605d8e0d824e91322047956fabd54cb50b02
cmrRose <cmr.rose@gmail.com>
2023-12-22 03:19:19 -0600

    start from existing simple leaflet.

---
+++ data.js
@@ -0,0 +1,71 @@
+//locations in geojson format 
+
+var mapdata = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#2b6c82",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "label": "New York"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96133422851562,
+          40.78626052122175
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#2c6355",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "label": "Rio de Janeiro"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -43.2015037536621,
+          -22.88661333314638
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#4f76a1",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "label": "London"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -0.07537007331848145,
+          51.50549704117305
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#3f928d",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "label": "Casablanca"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -7.605457305908203,
+          33.610330500723556
+        ]
+      }
+    }
+  ]
+}
