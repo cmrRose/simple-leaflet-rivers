b0e359d4a7b644b4a369358c1a2d8547b4b3ba5d
cmrRose <cmr.rose@gmail.com>
2023-12-22 03:51:10 -0600

    add project data

--- data.js
+++ data.js
@@ -6,66 +6,137 @@ var mapdata = {
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#2b6c82",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "label": "New York"
+        "Label": "Lake Winnebago"
       },
       "geometry": {
-        "type": "Point",
         "coordinates": [
-          -73.96133422851562,
-          40.78626052122175
-        ]
+          -88.41160781309985,
+          44.051410470077315
+        ],
+        "type": "Point"
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#2c6355",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "label": "Rio de Janeiro"
+        "Label": "Fox River"
       },
       "geometry": {
-        "type": "Point",
         "coordinates": [
-          -43.2015037536621,
-          -22.88661333314638
-        ]
+          -88.11083274756237,
+          44.395127878506486
+        ],
+        "type": "Point"
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#4f76a1",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "label": "London"
+        "Label": "Shawano Lake"
       },
       "geometry": {
-        "type": "Point",
         "coordinates": [
-          -0.07537007331848145,
-          51.50549704117305
-        ]
+          -88.5205685772428,
+          44.807904818348135
+        ],
+        "type": "Point"
       }
     },
     {
       "type": "Feature",
       "properties": {
-        "marker-color": "#3f928d",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "label": "Casablanca"
+        "Label": "Wolf River"
       },
       "geometry": {
-        "type": "Point",
         "coordinates": [
-          -7.605457305908203,
-          33.610330500723556
-        ]
+          -88.63013248688426,
+          44.96901167096894
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Label": "Peshtigo River"
+      },
+      "geometry": {
+        "coordinates": [
+          -88.24727356713142,
+          45.35515438821278
+        ],
+        "type": "Point"
       }
     }
   ]
 }
+//    
+//    {
+//  "type": "FeatureCollection",
+//  "features": [
+//    {
+//      "type": "Feature",
+//      "properties": {
+//        "marker-color": "#2b6c82",
+//        "marker-size": "medium",
+//        "marker-symbol": "",
+//        "label": "New York"
+//      },
+//      "geometry": {
+//        "type": "Point",
+//        "coordinates": [
+//          -73.96133422851562,
+//          40.78626052122175
+//        ]
+//      }
+//    },
+//    {
+//      "type": "Feature",
+//      "properties": {
+//        "marker-color": "#2c6355",
+//        "marker-size": "medium",
+//        "marker-symbol": "",
+//        "label": "Rio de Janeiro"
+//      },
+//      "geometry": {
+//        "type": "Point",
+//        "coordinates": [
+//          -43.2015037536621,
+//          -22.88661333314638
+//        ]
+//      }
+//    },
+//    {
+//      "type": "Feature",
+//      "properties": {
+//        "marker-color": "#4f76a1",
+//        "marker-size": "medium",
+//        "marker-symbol": "",
+//        "label": "London"
+//      },
+//      "geometry": {
+//        "type": "Point",
+//        "coordinates": [
+//          -0.07537007331848145,
+//          51.50549704117305
+//        ]
+//      }
+//    },
+//    {
+//      "type": "Feature",
+//      "properties": {
+//        "marker-color": "#3f928d",
+//        "marker-size": "medium",
+//        "marker-symbol": "",
+//        "label": "Casablanca"
+//      },
+//      "geometry": {
+//        "type": "Point",
+//        "coordinates": [
+//          -7.605457305908203,
+//          33.610330500723556
+//        ]
+//      }
+//    }
+//  ]
+//}
